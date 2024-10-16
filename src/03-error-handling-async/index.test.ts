1eaca1091a17182a9432cebdbf43007d8dd761a2
yourunb <bxyura@gmail.com>
2024-10-16 14:44:48 +0300

    refactor: correct error with js code in index

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -11,26 +11,26 @@ describe('resolveValue', () => {
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    const err = 'error';
+    const err = 'Provided error';
     
     expect(() => throwError(err)).toThrow(err);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    expect(() => throwError()).toThrow('Oops');
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    expect(() => throwCustomError()).toThrow('error');
+    expect(() => throwCustomError()).toThrow('This is my awesome custom error!');
     expect(() => throwCustomError()).toThrow(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    await expect(rejectCustomError()).rejects.toThrow('error');
+    await expect(rejectCustomError()).rejects.toThrow('This is my awesome custom error!');
     await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
