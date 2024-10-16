62bd63eaec639bed415ada741707591cdc4aec75
yourunb <bxyura@gmail.com>
2024-10-16 11:07:30 +0300

    feat: add tests for doStuffByInterval

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,6 +1,6 @@
 // Uncomment the code below and write your tests
 //import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
-import { doStuffByTimeout } from '.';
+import { doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => jest.useFakeTimers());
@@ -8,10 +8,10 @@ describe('doStuffByTimeout', () => {
 
   test('should set timeout with provided callback and timeout', () => {
     const callback = jest.fn();
-    const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+    const timeSpy = jest.spyOn(global, 'setTimeout');
 
     doStuffByTimeout(callback, 500);
-    expect(setTimeoutSpy).toHaveBeenCalledWith(callback, 500);
+    expect(timeSpy).toHaveBeenCalledWith(callback, 500);
   });
 
   test('should call callback only after timeout', () => {
@@ -28,25 +28,28 @@ describe('doStuffByTimeout', () => {
     expect(callback).toHaveBeenCalled();
   });
 });
-/*
-describe('doStuffByInterval', () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
-  });
 
-  afterAll(() => {
-    jest.useRealTimers();
-  });
+describe('doStuffByInterval', () => {
+  beforeAll(() => jest.useFakeTimers());
+  afterEach(() => jest.restoreAllMocks());
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const intervalSpy = jest.spyOn(global, 'setInterval');
+    const callback = jest.fn();
+
+    doStuffByInterval(callback, 500);
+    expect(intervalSpy).toHaveBeenCalledWith(callback, 500);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByInterval(callback, 500);
+    
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 });
-
+/*
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
     // Write your test here
